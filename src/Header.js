1ccdacc040ae23d3ca0f41d45842ad04c58dabd1
yves137 <oneg2981@gmail.com>
2023-10-05 15:15:29 +0200

    update project

--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,11 @@ export function Header() {
    return <div className="mb-10 text-blue-600">
         {Array(10)
           .fill(undefined)
-          .map((v, i) =>
-            (i===0) ? <><span className="underline">Home</span><span> | </span></> :
-            (i===9) ? <span className="underline">Exercise {i}</span> :
+          .map((_, i) =>
+            (i===0) ? <><span className="underline" key={`home${i}`}>Home</span><span key={`upper${i}`}> | </span></> :
+            (i===9) ? <span className="underline" key={`lastExercise${i}`}>Exercise {i}</span> :
            <>
-           <span className="underline">Exercise {i}</span> <span> | </span>
+           <span className="underline" key={`exercise${i}`}>Exercise {i}</span> <span key={`lower${i}`}> | </span>
            </> 
           )
           }
